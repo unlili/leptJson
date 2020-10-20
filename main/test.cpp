a8c87628aed5a83475e797e7113099cbbff1f156
Zihang <45307866+unlili@users.noreply.github.com>
2020-10-20 22:12:19 +0800

    Update test.cpp

--- main/test.cpp
+++ main/test.cpp
@@ -18,24 +18,133 @@ static int test_pass = 0;
         }\
     } while(0)
 
-#define EXPECT_EQ_INT(expect, actual) EXPECT_EQ_BASE((expect) == (actual), expect, actual, "%d")
+#define EXPECT_EQ_INT(expect, actual) EXPECT_EQ_BASE((expect) == (actual), expect, actual, "%d")  //%d会替换format
 
-static void test_parse_null() {
+//这个宏可以用来修改test_parse_invalid_value的四句重复语句
+#define TEST_ERROR(error,json)\
+		do{\
+			lept_value v;\
+			v.type = LEPT_FALSE;\
+			EXPECT_EQ_INT(error,lept_parse(&v,json));\
+			EXPECT_EQ_INT(LEPT_NULL,lept_get_type(&v));\
+		}while(0)
+
+#define EXPECT_EQ_DOUBLE(expect,actual) EXPECT_EQ_BASE((expect) == (actual), expect, actual, "%f")
+
+//这个宏用于测试解析number
+#define TEST_NUMBER(expect, json)\
+    do {\
+        lept_value v;\
+        EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, json));\
+        EXPECT_EQ_INT(LEPT_NUMBER, lept_get_type(&v));\
+        EXPECT_EQ_DOUBLE(expect, lept_get_number(&v));\
+    } while(0)
+
+//-------------------------每一种测试函数-------------------------
+//测试"" "  "
+static void test_parse_expect_value() {
+	lept_value v;
+
+	v.type = LEPT_FALSE;
+	EXPECT_EQ_INT(LEPT_PARSE_EXPECT_VALUE, lept_parse(&v, ""));
+	EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+
+	v.type = LEPT_FALSE;
+	EXPECT_EQ_INT(LEPT_PARSE_EXPECT_VALUE, lept_parse(&v, " "));
+	EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+}
+//测试无效的值
+static void test_parse_invalid_value()
+{
+
+	TEST_ERROR(LEPT_PARSE_INVALID_VALUE,"nul");
+	TEST_ERROR(LEPT_PARSE_INVALID_VALUE,"   dfgsdf");
+	/* invalid number */
+	TEST_ERROR(LEPT_PARSE_INVALID_VALUE, "+0");
+	TEST_ERROR(LEPT_PARSE_INVALID_VALUE, "+1");
+	TEST_ERROR(LEPT_PARSE_INVALID_VALUE, ".123");
+	TEST_ERROR(LEPT_PARSE_INVALID_VALUE, "1.");
+	TEST_ERROR(LEPT_PARSE_INVALID_VALUE, "INF");
+	TEST_ERROR(LEPT_PARSE_INVALID_VALUE, "inf");
+	TEST_ERROR(LEPT_PARSE_INVALID_VALUE, "NAN");
+	TEST_ERROR(LEPT_PARSE_INVALID_VALUE, "nan");
+
+}
+//测试一个值之后还有空格和东西
+static void test_parse_root_not_singular() {
+	lept_value v;
+	v.type = LEPT_FALSE;
+	EXPECT_EQ_INT(LEPT_PARSE_ROOT_NOT_SINGULAR, lept_parse(&v, "null x"));
+	EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+}
+//测试"true"
+static void test_parse_true()
+{
+	lept_value v;
+	v.type = LEPT_FALSE;
+	EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "true"));
+	EXPECT_EQ_INT(LEPT_TRUE, lept_get_type(&v));
+}
+//测试"false"
+static void test_parse_false()
+{
+	lept_value v;
+	v.type = LEPT_TRUE;
+	EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "false"));
+	EXPECT_EQ_INT(LEPT_FALSE, lept_get_type(&v));
+}
+//测试"null"
+static void test_parse_null()
+{
+	/*
+	lept_parse 会修改v.type的类型
+	lept_get_type 会获得v.type
+	*/
 	lept_value v;
 	v.type = LEPT_TRUE;
 	EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "null"));
 	EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+}
+//测试number
+static void test_parse_number()
+{
+	TEST_NUMBER(0.0, "0");
+	TEST_NUMBER(0.0, "-0");
+	TEST_NUMBER(0.0, "-0.0");
+	TEST_NUMBER(1.0, "1");
+	TEST_NUMBER(-1.0, "-1");
+	TEST_NUMBER(1.5,"1.5" );
+	TEST_NUMBER(3.1416, "3.1416");
+	TEST_NUMBER(1E10, "1E10");
+	TEST_NUMBER(1e10, "1e10");
+	TEST_NUMBER(1E+10, "1E+10");
+	TEST_NUMBER(1E-10, "1E-10");
+	TEST_NUMBER(-1E10, "-1E10");
+	TEST_NUMBER(-1e10, "-1e10");
+	TEST_NUMBER(-1E+10, "-1E+10");
+	TEST_NUMBER(-1E-10, "-1E-10");
+	TEST_NUMBER(1.234E+10, "1.234E+10");
+	TEST_NUMBER(1.234E-10, "1.234E-10");
+	TEST_NUMBER(0.0, "1e-10000"); /* must underflow */
+
 }
 
 /* ... */
 
 static void test_parse() {
 	test_parse_null();
+	test_parse_true();
+	test_parse_false();
+	test_parse_number();
+	test_parse_expect_value();
+	test_parse_invalid_value();
+	test_parse_root_not_singular();
 	/* ... */
 }
 
 int main() {
 	test_parse();
 	printf("%d/%d (%3.2f%%) passed\n", test_pass, test_count, test_pass * 100.0 / test_count);
+	system("pause");
 	return main_ret;
 }
